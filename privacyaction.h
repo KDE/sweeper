58304eea307d8815476bf0a049fa007f55bcdab7
Arto Hytönen <amthpublic@gmail.com>
2007-08-17 11:24:48 +0000

    this is the first commit that really remove all QString::null issues off kdeutil (for start, and to test it compiled in dashboard).
    not really sure why I can't compile myself a gcc-3.3 to my system, maybe it doesn't like my glibc-2.6.1, so can only test it with gcc-4.2 for now...
    
    svn path=/trunk/KDE/kdeutils/sweeper/; revision=701121

--- privacyaction.h
+++ privacyaction.h
@@ -28,7 +28,7 @@
 class PrivacyAction : public Q3CheckListItem
 {
    public:
-      PrivacyAction(K3ListViewItem * parent, QString name, QString desc = QString::null);	//krazy:exclude=nullstrassign for old broken gcc
+      PrivacyAction(K3ListViewItem * parent, QString name, QString desc = QString());
       ~PrivacyAction();
       
       void setDescription(const QString &desc);
