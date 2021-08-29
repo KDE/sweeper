99fc97cc1b1b9c4655955b194cee8a0320af6f7d
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-08-29 20:09:23 +0200

    Convert license headers to SPDX

--- privacyaction.h
+++ privacyaction.h
@@ -1,20 +1,8 @@
-/**
-  * Copyright (C) 2005 Brian S. Stephan ( bssteph at irtonline dot org )
-  *
-  *  This program is free software; you can redistribute it and/or modify
-  *  it under the terms of the GNU Lesser General Public License as published
-  *  by the Free Software Foundation; either version 2.1 of the License, or
-  *  (at your option) any later version.
-  *
-  *  This program is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU Lesser General Public License for more details.
-  *
-  *  You should have received a copy of the GNU Lesser General Public License
-  *  along with this program; if not, write to the Free Software
-  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-  */
+/*
+    SPDX-FileCopyrightText: 2005 Brian S. Stephan <bssteph@irtonline.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #ifndef PRIVACYACTION_H
 #define PRIVACYACTION_H
