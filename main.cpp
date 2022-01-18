69294022c4621394126e81ca43bf22a5b5cf8672
Laurent Montel <montel@kde.org>
2022-01-18 08:22:49 +0100

    Kdelibs4ConfigMigrator was removed in qt6

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@
 
 #include <KAboutData>
 #include <KCrash>
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 #include <Kdelibs4ConfigMigrator>
+#endif
 #include <KLocalizedString>
 
 #include <config-sweeper.h>
@@ -21,12 +23,13 @@ int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
    KLocalizedString::setApplicationDomain("sweeper");
-
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     Kdelibs4ConfigMigrator migrator(QStringLiteral("sweeper"));
     migrator.setConfigFiles(QStringList() << QStringLiteral("sweeperrc"));
     migrator.setConfigFiles(QStringList() << QStringLiteral("kprivacyrc"));
     migrator.setUiFiles(QStringList() << QStringLiteral("sweeperui.rc"));
     migrator.migrate();
+#endif
 
    KAboutData aboutData(QStringLiteral("sweeper"), i18n("Sweeper"),
                         QStringLiteral(SWEEPER_VERSION),
